7d6d3b9ec72c1ea5f475ae39df6cc69a55589f4d
geeky-aditya <adi.kr20004@gmail.com>
2026-09-20 23:41:58 +0530

    Update server for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import chatRoutes from "./routes/chat.js"
 import authRoutes from "./routes/auth.js";
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/api",chatRoutes);
 app.use("/api", authRoutes);
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
   console.log(`app is listening to ${PORT} port`);
   connectDB();
 })
